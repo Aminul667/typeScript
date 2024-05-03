256d851204d06345159e4065aeab95f7b5b2eaef
Aminul667 <rahathssn@gmail.com>
2024-05-04 02:13:14 +0600

    added intersection

--- module_1/src/1_10_unionIntersection.ts
+++ module_1/src/1_10_unionIntersection.ts
@@ -13,4 +13,23 @@
     gender: "Male",
     group: "C",
   };
+
+  //   intersection type
+  type FrontEnd = {
+    skills: string[];
+    designation1: "Front End";
+  };
+
+  type BackEnd = {
+    skills: string[];
+    designation2: "Back End";
+  };
+
+  type FullStack = FrontEnd & BackEnd;
+
+  const fullStack: FullStack = {
+    skills: ["HTML", "CSS", "JS"],
+    designation1: "Front End",
+    designation2: "Back End",
+  };
 }
