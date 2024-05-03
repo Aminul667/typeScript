17f1cb8e6b5d880f486c1ed2316c6db2880ca90e
Aminul667 <rahathssn@gmail.com>
2024-05-03 16:43:28 +0600

    basic datatypes

---
+++ module_1/src/1_4_basicDatatype.ts
@@ -0,0 +1,25 @@
+// Basic Data types
+
+// string
+let firstName: string = "Rahat"
+
+// number
+let roll: number = 30
+
+// boolean
+let isAdmin: boolean = true
+
+// undefined
+let x: undefined = undefined
+
+// null
+let y: null = null
+
+// array
+let myFriends : string[] = ['Sam', 'Bruno']
+
+let eligibleRoleList : number[] = [1, 3] 
+
+// tuple
+let coordinate : [number, number] = [1, 6]
+let ageName : [number, string, boolean] = [20, "Mr. X", true]
